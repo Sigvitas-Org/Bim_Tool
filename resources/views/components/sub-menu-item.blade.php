5cf7f3b51459485cd63eaa40281b95ad197ff08d
surya <spuranik@sigvitas.com>
2023-07-12 23:23:50 +0530

    First File Commit

--- resources/views/components/sub-menu-item.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- COLLAPSE - INFORMATION -->
-@if ($permission)
-    <a class="f-14 text-lightest" href="{{ $link }}" title="{{ $text }}">{{ $text }}</a>
-@endif
--- resources/views/notifications/all/new_user.blade.php
+++
@@ -1,2 +0,0 @@
-<x-cards.notification :notification="$notification"  link="javascript:;" :image="company()->logo_url"
-    :title="__('app.welcome') . ' ' . __('app.to') . ' ' . $companyName . ' !'" :time="$notification->created_at" />
